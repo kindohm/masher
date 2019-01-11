75c53f76f4513b7901f1ab6654073896ab4277fd
Calum Gunn <calum@calumgunn.com>
2019-01-11 19:22:47 +0100

    Convert sketch file

--- src/sketch.js
+++ src/sketch.js
@@ -1,100 +1,106 @@
-let bd, sd
-let colCount = 5
-let rowCount = 5
-let rows = []
+import 'p5/lib/addons/p5.sound';
+import BD from './sounds/bd.wav';
+import SD from './sounds/sd.wav';
 
-function preload() {
-  soundFormats('wav')
-  bd = loadSound('assets/bd.wav')
-  bd.setVolume(0.95)
-  bd.playMode('restart')
-  sd = loadSound('assets/sd.wav')
-  sd.setVolume(0.95)
-  sd.playMode('restart')
-}
+export default function sketch (p) {
+  let bd, sd;
+  let colCount = 5;
+  let rowCount = 5;
+  let rows = [];
 
-function setup() {
-  createCanvas(640, 480, WEBGL)
-}
+  p.preload = function() {
+    p.soundFormats('wav')
+    bd = p.loadSound(BD)
+    bd.setVolume(0.95)
+    bd.playMode('restart')
+    sd = p.loadSound(SD)
+    sd.setVolume(0.95)
+    sd.playMode('restart')
+  };
 
-function update() {
-  rows.forEach(row => {
-    row.forEach(thing => {
-      thing.y += thing.velocity.y
-      thing.velocity.y += thing.acceleration.y
+  p.setup = function() {
+    p.createCanvas(window.innerWidth, window.innerHeight, p.WEBGL)
+  }
+
+  p.update = function() {
+    rows.forEach(row => {
+      row.forEach(thing => {
+        thing.y += thing.velocity.y
+        thing.velocity.y += thing.acceleration.y
+      })
     })
-  })
-}
+  }
 
-function draw() {
-  update()
-  background('black')
-  directionalLight(250, 250, 250, 0.45, 0.25, -0.35)
-  translate((-colCount * 85) / 2, 0, -400)
+  p.draw = function() {
+    p.update()
+    p.background('black')
+    p.directionalLight(250, 250, 250, 0.45, 0.25, -0.35)
+    p.translate((-colCount * 85) / 2, 0, -400)
 
-  rows.forEach(row => {
-    row.forEach(thing => {
-      const color = thing.color
-      noStroke()
-      ambientMaterial(color.red, color.green, color.blue)
+    rows.forEach(row => {
+      row.forEach(thing => {
+        const color = thing.color
+        p.noStroke()
+        p.ambientMaterial(color.red, color.green, color.blue)
 
-      translate(thing.x, thing.y, thing.z)
-      box(thing.width, thing.height, thing.width)
-      translate(-thing.x, -thing.y, -thing.z)
+        p.translate(thing.x, thing.y, thing.z)
+        p.box(thing.width, thing.height, thing.width)
+        p.translate(-thing.x, -thing.y, -thing.z)
+      })
     })
-  })
 
-  translate((colCount * 85) / 2, 0, 400)
-}
+    p.translate((colCount * 85) / 2, 0, 400)
+  }
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min
-}
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min
+  }
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
 
-function trigger() {
-  rows = Array(rowCount)
-    .fill(null)
-    .map((x, rowIndex) => {
-      return Array(colCount)
-        .fill(null)
-        .map((val, index) => {
-          return {
-            x: index * 110,
-            y: 170,
-            z: rowIndex * 110,
-            width: 80,
-            height: 80,
-            color: {
-              red: getRandomIntInclusive(0, 255),
-              green: getRandomIntInclusive(0, 255),
-              blue: getRandomIntInclusive(0, 255)
-            },
-            velocity: {
-              y: 0
-            },
-            acceleration: {
-              y: getRandomArbitrary(-0.03, -0.2)
+  function trigger() {
+    rows = Array(rowCount)
+      .fill(null)
+      .map((x, rowIndex) => {
+        return Array(colCount)
+          .fill(null)
+          .map((val, index) => {
+            return {
+              x: index * 110,
+              y: 170,
+              z: rowIndex * 110,
+              width: 80,
+              height: 80,
+              color: {
+                red: getRandomIntInclusive(0, 255),
+                green: getRandomIntInclusive(0, 255),
+                blue: getRandomIntInclusive(0, 255)
+              },
+              velocity: {
+                y: 0
+              },
+              acceleration: {
+                y: getRandomArbitrary(-0.03, -0.2)
+              }
             }
-          }
-        })
-    })
+          })
+      })
 
-  bd.play()
-  sd.play()
-}
+    bd.play()
+    sd.play()
+  }
 
-function mouseClicked() {
-  trigger()
-}
-
-function keyPressed() {
-  if (keyCode === 32) {
+  p.mouseClicked = function() {
     trigger()
+  }
+
+  p.keyPressed = function() {
+    if (p.keyCode === 32) {
+      trigger()
+    }
   }
 }
