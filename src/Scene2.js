8eea39760bb065a978a689833745e87622c854e8
Mike Hodnick <michael_hodnick@crgl-thirdparty.com>
2019-01-15 14:11:27 -0600

    trigger drums in Scene2

--- src/Scene2.js
+++ src/Scene2.js
@@ -1,6 +1,11 @@
 import 'p5/lib/addons/p5.sound';
 import { getRandomIntInclusive, getRandomArbitrary } from './Random';
 
+import Bd1 from './sounds/like/bd1.mp3';
+import Bd2 from './sounds/like/bd2.mp3';
+import Cp1 from './sounds/like/cp1.mp3';
+import Cp2 from './sounds/like/cp2.mp3';
+
 export default function Scene2(p) {
   const max = 25;
   const size = 1;
@@ -10,7 +15,29 @@ export default function Scene2(p) {
   const halfPi = p.PI / 2;
   const negHalfPi = -p.PI / 2;
   let cameraRotation = 0;
-  p.preload = function() {};
+  let otherBank;
+
+  let bd1, bd2, cp1, cp2;
+
+  p.preload = function() {
+    p.soundFormats('mp3');
+
+    bd1 = p.loadSound(Bd1);
+    bd1.setVolume(0.95);
+    bd1.playMode('restart');
+
+    bd2 = p.loadSound(Bd2);
+    bd2.setVolume(0.95);
+    bd2.playMode('restart');
+
+    cp1 = p.loadSound(Cp1);
+    cp1.setVolume(0.95);
+    cp1.playMode('restart');
+
+    cp2 = p.loadSound(Cp2);
+    cp2.setVolume(0.95);
+    cp2.playMode('restart');
+  };
 
   p.setup = function() {
     p.createCanvas(window.innerWidth, window.innerHeight, p.WEBGL);
@@ -128,6 +155,20 @@ export default function Scene2(p) {
 
     nodes[index] = newNode;
     index++;
+
+    if (accent) {
+      if (otherBank) {
+        cp2.play();
+      } else {
+        cp1.play();
+      }
+    } else {
+      if (otherBank) {
+        bd2.play();
+      } else {
+        bd1.play();
+      }
+    }
   }
 
   p.keyPressed = function() {
@@ -137,6 +178,16 @@ export default function Scene2(p) {
 
     if (p.keyCode === 88) {
       return trigger({ accent: true });
+    }
+
+    // 1 = bank1
+    if (p.keyCode === 49) {
+      otherBank = false;
+    }
+
+    // 2 = bank2
+    if (p.keyCode === 50) {
+      otherBank = true;
     }
   };
 }
